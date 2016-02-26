aa079fb417d9ceca8ce216b2397b745c65cd0ccb
Anatoli Angelov <anatoli.angelov@hotmail.com>
2016-02-26 15:04:16 +0200

    Implemented repository pattern, installed Glimpse for debugging.

---
+++ source/YouTubePlaylists.Data/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace YouTubePlaylists.Data.Contracts
+{
+    using System;
+    using System.Linq;
+
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        IQueryable<T> All();
+
+        T Find(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        int SaveChanges();
+    }
+}
